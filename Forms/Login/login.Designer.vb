ad76593783882940c2f93176039ee0256d7b3bb6
jailenannm-web <jailenannm@gmail.com>
2025-11-01 15:35:15 +0800

    user access level

--- Forms/Login/login.Designer.vb
+++ Forms/Login/login.Designer.vb
@@ -36,6 +36,9 @@ Partial Class Login
         '
         'Panel1
         '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(246, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(222, Byte), Integer))
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.Controls.Add(Me.btn_Login)
@@ -45,40 +48,46 @@ Partial Class Login
         Me.Panel1.Controls.Add(Me.Label7)
         Me.Panel1.Controls.Add(Me.Label8)
         Me.Panel1.Controls.Add(Me.txb_Username)
-        Me.Panel1.Location = New System.Drawing.Point(994, 37)
+        Me.Panel1.Location = New System.Drawing.Point(1325, 46)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(584, 818)
+        Me.Panel1.Size = New System.Drawing.Size(779, 1007)
         Me.Panel1.TabIndex = 4
         '
         'Label1
         '
-        Me.Label1.Font = New System.Drawing.Font("Poppins", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(184, 115)
+        Me.Label1.Location = New System.Drawing.Point(245, 142)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(256, 105)
+        Me.Label1.Size = New System.Drawing.Size(341, 129)
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "LOGIN"
         '
         'btn_Login
         '
+        Me.btn_Login.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btn_Login.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btn_Login.Font = New System.Drawing.Font("Poppins", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Login.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Login.ForeColor = System.Drawing.Color.White
-        Me.btn_Login.Location = New System.Drawing.Point(350, 673)
+        Me.btn_Login.Location = New System.Drawing.Point(467, 828)
+        Me.btn_Login.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn_Login.Name = "btn_Login"
-        Me.btn_Login.Size = New System.Drawing.Size(152, 49)
+        Me.btn_Login.Size = New System.Drawing.Size(203, 60)
         Me.btn_Login.TabIndex = 7
         Me.btn_Login.Text = "Login"
         Me.btn_Login.UseVisualStyleBackColor = False
         '
         'btn_Cancel
         '
-        Me.btn_Cancel.Font = New System.Drawing.Font("Poppins", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Cancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_Cancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Cancel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btn_Cancel.Location = New System.Drawing.Point(76, 673)
+        Me.btn_Cancel.Location = New System.Drawing.Point(101, 828)
+        Me.btn_Cancel.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn_Cancel.Name = "btn_Cancel"
-        Me.btn_Cancel.Size = New System.Drawing.Size(154, 49)
+        Me.btn_Cancel.Size = New System.Drawing.Size(205, 60)
         Me.btn_Cancel.TabIndex = 6
         Me.btn_Cancel.Text = "Cancel"
         Me.btn_Cancel.UseVisualStyleBackColor = True
@@ -86,60 +95,68 @@ Partial Class Login
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Poppins", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.Label6.Location = New System.Drawing.Point(194, 378)
+        Me.Label6.Location = New System.Drawing.Point(259, 465)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(197, 56)
+        Me.Label6.Size = New System.Drawing.Size(211, 46)
         Me.Label6.TabIndex = 5
         Me.Label6.Text = "Username"
         '
         'txb_Password
         '
+        Me.txb_Password.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txb_Password.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.txb_Password.Font = New System.Drawing.Font("Poppins", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txb_Password.Location = New System.Drawing.Point(94, 488)
+        Me.txb_Password.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txb_Password.Location = New System.Drawing.Point(125, 601)
+        Me.txb_Password.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txb_Password.Multiline = True
         Me.txb_Password.Name = "txb_Password"
-        Me.txb_Password.Size = New System.Drawing.Size(384, 42)
+        Me.txb_Password.Size = New System.Drawing.Size(512, 52)
         Me.txb_Password.TabIndex = 4
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(161, 613)
+        Me.Label7.Location = New System.Drawing.Point(215, 754)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(0, 13)
+        Me.Label7.Size = New System.Drawing.Size(0, 16)
         Me.Label7.TabIndex = 3
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Font = New System.Drawing.Font("Poppins", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.Label8.Location = New System.Drawing.Point(205, 533)
+        Me.Label8.Location = New System.Drawing.Point(273, 656)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(186, 56)
+        Me.Label8.Size = New System.Drawing.Size(204, 46)
         Me.Label8.TabIndex = 2
         Me.Label8.Text = "Password"
         '
         'txb_Username
         '
+        Me.txb_Username.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txb_Username.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.txb_Username.Font = New System.Drawing.Font("Poppins", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txb_Username.Location = New System.Drawing.Point(94, 333)
+        Me.txb_Username.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txb_Username.Location = New System.Drawing.Point(125, 410)
+        Me.txb_Username.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txb_Username.Multiline = True
         Me.txb_Username.Name = "txb_Username"
-        Me.txb_Username.Size = New System.Drawing.Size(384, 42)
+        Me.txb_Username.Size = New System.Drawing.Size(512, 52)
         Me.txb_Username.TabIndex = 1
         '
         'Login
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.BackgroundImage = Global.StaCruzPropertyCustodianSystem.My.Resources.Resources.BG1
-        Me.ClientSize = New System.Drawing.Size(1614, 891)
+        Me.ClientSize = New System.Drawing.Size(1924, 1097)
         Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Login"
         Me.Text = "Login"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
--- Forms/UserAccess/UserLevel.Designer.vb
+++ Forms/UserAccess/UserLevel.Designer.vb
@@ -39,31 +39,35 @@ Partial Class UserLevel
         'Label2
         '
         Me.Label2.BackColor = System.Drawing.Color.WhiteSmoke
-        Me.Label2.Font = New System.Drawing.Font("Poppins", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(246, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(222, Byte), Integer))
-        Me.Label2.Location = New System.Drawing.Point(157, 546)
+        Me.Label2.Location = New System.Drawing.Point(209, 672)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(590, 115)
+        Me.Label2.Size = New System.Drawing.Size(787, 142)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "STA CRUZ PROPERTY"
         '
         'Label3
         '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Label3.BackColor = System.Drawing.Color.WhiteSmoke
-        Me.Label3.Font = New System.Drawing.Font("Poppins ExtraBold", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(246, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(222, Byte), Integer))
-        Me.Label3.Location = New System.Drawing.Point(147, 623)
+        Me.Label3.Location = New System.Drawing.Point(196, 767)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(600, 92)
+        Me.Label3.Size = New System.Drawing.Size(800, 113)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = " CUSTODIAN SYSTEM"
         '
         'Label4
         '
         Me.Label4.Image = Global.StaCruzPropertyCustodianSystem.My.Resources.Resources.logo2_removebg_preview
-        Me.Label4.Location = New System.Drawing.Point(315, 258)
+        Me.Label4.Location = New System.Drawing.Point(393, 322)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(298, 266)
+        Me.Label4.Size = New System.Drawing.Size(397, 327)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "."
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -76,78 +80,93 @@ Partial Class UserLevel
         Me.Panel1.Controls.Add(Me.btn_Admin)
         Me.Panel1.Controls.Add(Me.btn_SuperAdmin)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(962, 34)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel1.Location = New System.Drawing.Point(1114, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(584, 818)
+        Me.Panel1.Size = New System.Drawing.Size(810, 1097)
         Me.Panel1.TabIndex = 0
         '
         'Label5
         '
-        Me.Label5.Font = New System.Drawing.Font("Poppins ExtraBold", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.Label5.Location = New System.Drawing.Point(193, 153)
+        Me.Label5.Location = New System.Drawing.Point(257, 188)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(246, 99)
+        Me.Label5.Size = New System.Drawing.Size(328, 122)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "LEVEL"
         '
         'btn_Staff
         '
+        Me.btn_Staff.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btn_Staff.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btn_Staff.Font = New System.Drawing.Font("Poppins", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Staff.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Staff.ForeColor = System.Drawing.Color.White
-        Me.btn_Staff.Location = New System.Drawing.Point(109, 630)
+        Me.btn_Staff.Location = New System.Drawing.Point(176, 865)
+        Me.btn_Staff.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_Staff.Name = "btn_Staff"
-        Me.btn_Staff.Size = New System.Drawing.Size(373, 75)
+        Me.btn_Staff.Size = New System.Drawing.Size(497, 92)
         Me.btn_Staff.TabIndex = 3
         Me.btn_Staff.Text = "STAFF"
         Me.btn_Staff.UseVisualStyleBackColor = False
         '
         'btn_Admin
         '
+        Me.btn_Admin.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btn_Admin.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btn_Admin.Font = New System.Drawing.Font("Poppins", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Admin.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_Admin.ForeColor = System.Drawing.Color.White
-        Me.btn_Admin.Location = New System.Drawing.Point(109, 489)
+        Me.btn_Admin.Location = New System.Drawing.Point(176, 692)
+        Me.btn_Admin.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_Admin.Name = "btn_Admin"
-        Me.btn_Admin.Size = New System.Drawing.Size(373, 75)
+        Me.btn_Admin.Size = New System.Drawing.Size(497, 92)
         Me.btn_Admin.TabIndex = 2
         Me.btn_Admin.Text = "ADMIN"
         Me.btn_Admin.UseVisualStyleBackColor = False
         '
         'btn_SuperAdmin
         '
+        Me.btn_SuperAdmin.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btn_SuperAdmin.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.btn_SuperAdmin.Font = New System.Drawing.Font("Poppins", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_SuperAdmin.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_SuperAdmin.ForeColor = System.Drawing.Color.White
-        Me.btn_SuperAdmin.Location = New System.Drawing.Point(109, 345)
+        Me.btn_SuperAdmin.Location = New System.Drawing.Point(176, 515)
+        Me.btn_SuperAdmin.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_SuperAdmin.Name = "btn_SuperAdmin"
-        Me.btn_SuperAdmin.Size = New System.Drawing.Size(373, 75)
+        Me.btn_SuperAdmin.Size = New System.Drawing.Size(497, 92)
         Me.btn_SuperAdmin.TabIndex = 1
         Me.btn_SuperAdmin.Text = "SUPER ADMIN"
         Me.btn_SuperAdmin.UseVisualStyleBackColor = False
         '
         'Label1
         '
-        Me.Label1.Font = New System.Drawing.Font("Poppins ExtraBold", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(83, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(57, 75)
+        Me.Label1.Location = New System.Drawing.Point(76, 92)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(508, 92)
+        Me.Label1.Size = New System.Drawing.Size(677, 113)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "USER ACCESS"
         '
         'UserLevel
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.BackgroundImage = Global.StaCruzPropertyCustodianSystem.My.Resources.Resources.BG1
-        Me.ClientSize = New System.Drawing.Size(1614, 891)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1924, 1097)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "UserLevel"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.Panel1.ResumeLayout(False)
