75369712e068dff7cdb4c99277798964c7708fcc
jailenannm-web <jailenannm@gmail.com>
2025-12-18 01:26:21 +0800

    audit button

--- Forms/Admin/audit.vb
+++ Forms/Admin/audit.vb
@@ -1,3 +1,10 @@
-﻿Public Class audit
+﻿Imports System
+
+Public Class audit
+    Private Sub btnSave_Click(sender As Object, e As System.EventArgs) Handles btnSave.Click
+        Dim frm As New AuditReport()
+        frm.Show()
+    End Sub
+
 
 End Class
