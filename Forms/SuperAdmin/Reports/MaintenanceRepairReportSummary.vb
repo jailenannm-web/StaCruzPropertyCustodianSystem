0c6075973ce2fa7784f3b61676828a3f147b0e3a
krsxianley <christianneleyubana30@gmail.com>
2025-12-29 14:52:53 +0800

    reports summary

---
+++ Forms/SuperAdmin/Reports/MaintenanceRepairReportSummary.vb
@@ -0,0 +1,3 @@
+﻿Public Class MaintenanceRepairReportSummary
+
+End Class
---
+++ Forms/SuperAdmin/Reports/UserReportSummary.vb
@@ -0,0 +1,65 @@
+﻿Public Class UserReportSummary
+    Private Sub Panel1_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub UserReportSummary_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As System.EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As System.EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As System.EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As System.EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As System.EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As System.EventArgs) Handles school.TextChanged
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As System.EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As System.EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As System.EventArgs) Handles dateReport.ValueChanged
+
+    End Sub
+End Class
