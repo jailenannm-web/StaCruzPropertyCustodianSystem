94e1c86fad1ad457f1e6edd3a2b9c8b948dee74e
krsxianley <christianneleyubana30@gmail.com>
2025-12-29 15:41:38 +0800

    jckyfiyt

--- Forms/SuperAdmin/Reports/PropertyRequestReportSummary.vb
+++ Forms/SuperAdmin/Reports/PropertyRequestReportSummary.vb
@@ -2,4 +2,8 @@
     Private Sub DataGridView1_CellContentClick(sender As Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
+
+    Private Sub PropertyRequestReportSummary_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
