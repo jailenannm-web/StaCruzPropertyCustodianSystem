9f9c323e03a1c76a88fa959092ec45fca86cab17
krsxianley <christianneleyubana30@gmail.com>
2026-01-04 15:39:18 +0800

    Maintenance, Supply, Property Summary

---
+++ Forms/SuperAdmin/Reports/MaintenanceRequestSummaryReport.vb
@@ -0,0 +1,9 @@
+﻿Public Class MaintenanceRequestSummaryReport
+    Private Sub maintenanceRequest_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles maintenanceRequest.Paint
+
+    End Sub
+
+    Private Sub MaintenanceRequestSummaryReport_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Forms/SuperAdmin/Reports/SupplyRequestSummary.vb
@@ -0,0 +1,9 @@
+﻿Public Class SupplyRequestSummary
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub btn_Back_Click(sender As Object, e As System.EventArgs) Handles btn_Back.Click
+
+    End Sub
+End Class
